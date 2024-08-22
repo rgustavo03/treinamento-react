ca13a67080cbedec8d0ce9c83300887b3ad6ea94
tupinambajunior <tupinambajunior2@gmail.com>
2024-08-22 11:45:33 -0300

    remocao comentarios

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,6 @@ function App() {
 
 
   useEffect(() => {
-    //console.log('INICIOU')
     inicializar();
   }, [])
 
@@ -65,93 +64,7 @@ function App() {
     setCpf('');
   }, [tipoCliente]);
 
-
-  // useEffect(() => {
-  //   const listaFiltrada = listaClientes
-  //     .filter(x =>
-  //       x.cpf.toUpperCase().includes(filtroDocumento.toUpperCase()));
-  //   setListaClientesFiltrada(listaFiltrada);
-  // }, [filtroDocumento, listaClientes ]);
-
-  /* 
-    - Nome
-    - Data de Nascimento
-    - CPF
-    - Email
-  */
-
-  //map (mapeamento) -> moficação (um novo array com a mesma quantidade de objetos), 
-  //filter -> filtrar (array de acordo com o filtro), 
-  //find -> filtrar (retorna um objeto)
-  //reduce -> reduzir (apenas um valor)
-  //sort -> ordenação
-
-
-
-  // const usuarios = [
-  //   { id: 1, nome: 'joao', idade: 18, pontos: 600 },
-  //   { id: 2, nome: 'antonio', idade: 10, pontos: 500 },
-  //   { id: 3, nome: 'pedro', idade: 15, pontos: 300 },
-  //   { id: 4, nome: 'lucas', idade: 20, pontos: 150 },
-  //   { id: 5, nome: 'maria', idade: 5, pontos: 100 },
-  // ];
-
-  // const rankingUsuarios = usuarios.sort((a, b) => b.pontos - a.pontos);
-  // console.log('rankingUsuarios =>', rankingUsuarios);
-
-  // const rankingUsuariosAdultos = usuarios
-  //   .filter(u => u.idade >= 18)
-  //   .sort((a, b) => b.pontos - a.pontos)
-  //   .map(u => {
-  //     return {
-  //       nome: u.nome,
-  //       pontos: u.pontos,
-  //       recompensa: u.pontos * 2.5
-  //     }
-  //   });
-
-
-
-  //pop -> remove o ultimo item do array
-  //push -> adiciona um item ao final do array
-  //shift -> remove o primeiro item do array
-  //unshift -> adiciona um item no inicio do array
-
-  // console.log('rankingUsuariosAdultos =>', rankingUsuariosAdultos);
-
-  // const usuariosAdultos = usuarios.filter(usuario => usuario.idade >= 18);
-  // console.log('usuariosAdultos =>', usuariosAdultos);
-
-  // const usuario3 = usuarios.find(usuario => usuario.id == 3);
-  // console.log('usuario3 =>', usuario3);
-  // //const, let
-
-  // const nomeEquipe = usuarios.reduce((valorAnterior, elementoAtual) => valorAnterior + ', ' + elementoAtual.nome, '');
-
-  // console.log('nomeEquipe', nomeEquipe);
-
-  // let pontuacaoTotal = usuarios
-  //   .reduce(function (valorAnterior, elementoAtual) {
-  //     console.log('valorAnterior: ', valorAnterior)
-  //     console.log('elementoAtual: ', elementoAtual)
-  //     return valorAnterior + elementoAtual.pontos
-  //   }, 0);
-
-  //let pontuacaoTotal = usuarios.reduce((valorAnterior, elementoAtual) => valorAnterior + elementoAtual.pontos, 0);
-
-  // let pontuacaoTotal = 0;
-
-  // for (let index = 0; index < usuarios.length; index++) {
-  //   pontuacaoTotal += usuarios[index].pontos;
-  // }
-
-  //console.log('pontuacaoTotal => ', pontuacaoTotal);
-
-  //react => reativo (ESTADO)
-
   function inicializar() {
-    //setNome('Joao')
-
     const clientesStorage = localStorage.getItem('listaCliente');
 
     const listaClientesStorage = clientesStorage ? JSON.parse(clientesStorage) : [];
@@ -160,17 +73,8 @@ function App() {
       const clientesDefault = [];
 
       for (let index = 0; index < 10; index++) {
-        // clientesDefault.push({
-        //   id: uuidv4(),
-        //   cpf: ''.padStart(11, index.toString()),
-        //   nome: `cliente ${index}`, //template string,
-        //   email: 'cliente' + index.toString() + '@email.com',
-        //   dataNascimento: '1900-01-' + (index + 1).toString().padStart(2, '0')
-        // })
-
         clientesDefault.push({
           id: uuidv4(),
-          //cpf: ''.padStart(11, index.toString()),
           cpf: index % 2 == 0 ? ''.padStart(11, '1') : ''.padStart(14, '2'),
           nome: faker.person.fullName(),
           email: faker.internet.email(),
@@ -196,16 +100,6 @@ function App() {
 
     const novoArray = listaClientes.filter(c => c.id != id);
     setListaClientes(novoArray);
-
-    // const novoArray = [];
-    // listaClientes.forEach(element => {
-    //   if(element.id == id)
-    //     return;
-
-    //   novoArray.push(element);
-    // });
-
-    // setListaClientes(novoArray);
   }
 
   function editar(id) {
@@ -235,12 +129,6 @@ function App() {
   }
 
   function salvar() {
-
-    //falsy values
-
-    //tipadas true false
-    //false: false, '', null, undefined, 
-
     if (!nome) {
       alert('preencha o campo nome');
       return;
@@ -252,15 +140,6 @@ function App() {
       alert('já existe outro cliente com o mesmo nome');
       return;
     }
-
-    // for (let index = 0; index < listaClientes.length; index++) {
-    //   const element = listaClientes[index];
-
-    //   if(element.nome == nome) {
-    //     alert('já existe outro cliente com o mesmo nome');
-    //     return;
-    //   }
-    // }
 
     if (tipoCliente == 'PF' && !dataNascimento) {
       alert('preencha o campo data nascimento');
@@ -316,7 +195,6 @@ function App() {
     console.log('listaClientes =>', listaClientes)
 
     if (idCliente) {
-      //remover da lista o cliente que estou editando
       let novaLista = listaClientes.filter(c => c.id != idCliente);
 
       setListaClientes([...novaLista, dados]);
@@ -324,10 +202,6 @@ function App() {
       setListaClientes([...listaClientes, dados]);
     }
   }
-
-
-  // console.log('RENDERIZOU OS COMPONENTES');
-  // console.log('listaClientes =>', listaClientes)
 
   console.log(tipoCliente)
 
@@ -339,11 +213,6 @@ function App() {
           titulo="Cadastro de Clientes"
           subtitulo={`${listaClientes.length} registros`}
           icone="fa-solid fa-user-group" />
-
-        {/* {listaClientes.length == 0 && <Button
-          onClick={inicializar}
-          nome="Incializar"
-          disabled={listaClientes.length > 0} />} */}
 
         {idCliente && <div className='alert alert-warning my-4'>
           Id Cliente selecionado: {idCliente}
@@ -439,9 +308,7 @@ function App() {
           </div>
 
           <div className='col-lg-8'>
-            <div className="my-3 p-3 bg-body rounded shadow-sm"
-            // style={{maxHeight: '400px', overflowX: 'hidden', overflowY: 'scroll'}}
-            >
+            <div className="my-3 p-3 bg-body rounded shadow-sm">
               <div className='row'>
                 <div className='col-7'>
                   <Input
@@ -510,23 +377,6 @@ function App() {
             </div>
           </div>
         </div>
-
-
-        {/* <div className='row' style={{ height: '50px' }}>
-          <div className='col-xxl-1 col-xl-2 col-lg-3 col-md-4 col-sm-5 bg-primary' style={{ height: '50px' }}>1</div>
-          <div className='col-xxl-1 col-xl-2 col-lg-3 col-md-4 col-sm-5 bg-secondary' style={{ height: '50px' }}>2</div>
-          <div className='col-xxl-1 col-xl-2 col-lg-3 col-md-4 col-sm-5 bg-success' style={{ height: '50px' }}>3</div>
-          <div className='col-xxl-1 col-xl-2 col-lg-3 col-md-4 col-sm-5 bg-danger' style={{ height: '50px' }}>4</div>
-          <div className='col-xxl-1 col-xl-2 col-lg-3 col-md-4 col-sm-5 bg-warning' style={{ height: '50px' }}>5</div>
-          <div className='col-xxl-1 col-xl-2 col-lg-3 col-md-4 col-sm-5 bg-info' style={{ height: '50px' }}>6</div>
-          <div className='col-xxl-1 col-xl-2 col-lg-3 col-md-4 col-sm-5 bg-light' style={{ height: '50px' }}>7</div>
-          <div className='col-xxl-1 col-xl-2 col-lg-3 col-md-4 col-sm-5 bg-dark' style={{ height: '50px' }}>8</div>
-          <div className='col-xxl-1 col-xl-2 col-lg-3 col-md-4 col-sm-5 bg-primary' style={{ height: '50px' }}>9</div>
-          <div className='col-xxl-1 col-xl-2 col-lg-3 col-md-4 col-sm-5 bg-secondary' style={{ height: '50px' }}>10</div>
-          <div className='col-xxl-1 col-xl-2 col-lg-3 col-md-4 col-sm-5 bg-danger' style={{ height: '50px' }}>11</div>
-          <div className='col-xxl-1 col-xl-2 col-lg-3 col-md-4 col-sm-5 bg-warning' style={{ height: '50px' }}>12</div>
-        </div> */}
-
       </div>
     </div>
   )
